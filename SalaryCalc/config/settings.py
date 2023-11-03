605697a1eb07c13a0ea23b0d2895a1f0d0dbbb7e
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-11-04 01:00:05 +0400

    change-pass, email_confirm, verification_sent

--- SalaryCalc/config/settings.py
+++ SalaryCalc/config/settings.py
@@ -183,7 +183,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'tr'
+LANGUAGE_CODE = 'az'
 
 TIME_ZONE = 'Asia/Baku'
 
