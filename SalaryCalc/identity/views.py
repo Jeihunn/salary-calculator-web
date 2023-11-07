4620d228457bcb9756f3e32f99be7d3becf51b43
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-11-07 22:49:32 +0400

    allauth urls and settings.py config

--- SalaryCalc/identity/views.py
+++ SalaryCalc/identity/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
-
+@login_required
 def profile_view(request):
     return render(request, 'identity/profile.html')
