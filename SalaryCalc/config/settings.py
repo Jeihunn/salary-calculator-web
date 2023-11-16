5d9a28842877fd7bb4877b6640152a51bca88e34
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-11-17 00:13:57 +0400

    EMAIL_HOST_USER changed

--- SalaryCalc/config/settings.py
+++ SalaryCalc/config/settings.py
@@ -126,8 +126,8 @@ LOGIN_URL = 'account_login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pavshop.project@gmail.com'
-EMAIL_HOST_PASSWORD = 'vfvawfeqwoaaergj'
+EMAIL_HOST_USER = 'maashesabla@gmail.com'
+EMAIL_HOST_PASSWORD = 'vbkt egmu aqrb jrhj'
 EMAIL_PORT = 587
 # # Yandex config
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
