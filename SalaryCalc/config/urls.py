9e7a995283ee30d8e3963b8ace79229198153648
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-11-29 13:39:40 +0400

    add sitemaps

--- SalaryCalc/config/urls.py
+++ SalaryCalc/config/urls.py
@@ -18,8 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+from core.sitemaps import IndexViewSitemap, OtherPageSitemap
+
+sitemaps = {
+    'index': IndexViewSitemap,
+    'otherpage': OtherPageSitemap,
+}
 
 urlpatterns = [
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    
     path('adm1n/', admin.site.urls),
     path("", include("identity.urls")),
     path("", include("core.urls", namespace="core")),
